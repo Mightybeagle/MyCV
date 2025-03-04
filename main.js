c8cf6881467d304edf6b6b8a17a1b44a5c6708a1
AlexM <84864428+Mightybeagle@users.noreply.github.com>
2025-03-04 14:49:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+console.log("JS Working...")
+
+//proficient Dropdown
+$(document).ready(function(){
+  $("#proficient").slideUp("slow");
+  $("#proficientTitle").click(function(){
+    $("#proficient").slideToggle("slow");
+  });
+});
+
+//experience Dropdown
+$(document).ready(function(){
+  $("#experience").slideUp("slow");
+  $("#experienceTitle").click(function(){
+    $("#experience").slideToggle("slow");
+  });
+});
+
+//iTSkills Dropdown
+$(document).ready(function(){
+  $("#iTSkills").slideUp("slow");
+  $("#iTSkillsTitle").click(function(){
+    $("#iTSkills").slideToggle("slow");
+  });
+});
+
+//Education Dropdown
+$(document).ready(function(){
+  $("#education").slideUp("slow");
+  $("#educationTitle").click(function(){
+    $("#education").slideToggle("slow");
+  });
+});
+
+//WorkExp Dropdown
+$(document).ready(function(){
+  $("#WorkExp").slideUp("slow");
+  $("#WorkExpTitle").click(function(){
+    $("#WorkExp").slideToggle("slow");
+  });
+});
